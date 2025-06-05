d806de60096fac48af68e12a2825d98f21ff37cc
Natalia Taira <nataliagtaira@gmail.com>
2025-06-05 20:23:43 -0300

    Implementa interface repository e duas funções auxiliares que realizam toda operação CRUD da Conta suas classes filhas.

---
+++ src/controller/ContaController.ts
@@ -0,0 +1,100 @@
+import { Conta } from "../model/Conta";
+import { ContaRepository } from "../repository/ContaRepository";
+import { colors } from "../util/Colors";
+
+export class ContaController implements ContaRepository {
+
+    private numero: number = 0;
+    private listaContas: Array<Conta> = new Array<Conta>();
+
+    public procurarPorNumeroConta(numero: number): void {
+        let buscaConta = this.buscarNoArray(numero);
+        if (buscaConta != null) {
+            buscaConta.visualizar();
+        } else
+            console.log(colors.fg.red, `\nA Conta numero: ${numero} não foi encontrada!`, colors.reset);
+    }
+
+    public listarTodas(): void {
+        for (let conta of this.listaContas) {
+            conta.visualizar();
+        };
+    }
+
+    public cadastrar(conta: Conta): void {
+        this.listaContas.push(conta);
+        console.log(colors.fg.green, `\nA Conta número: ${conta.numero} foi criada com sucesso!`, colors.reset);
+    }
+
+    public atualizar(conta: Conta): void {
+        let buscaConta = this.buscarNoArray(conta.numero);
+
+        if (buscaConta != null) {
+            this.listaContas[this.listaContas.indexOf(buscaConta)] = conta;
+            console.log(colors.fg.green, `\nA Conta numero: ${conta.numero} foi atualizada com sucesso!`, colors.reset);
+        } else
+            console.log(colors.fg.red, `\nA Conta numero: ${conta.numero} não foi encontrada!`, colors.reset);
+    }
+
+    public deletar(numero: number): void {
+        let buscaConta = this.buscarNoArray(numero);
+
+        if (buscaConta != null) {
+            this.listaContas.splice(this.listaContas.indexOf(buscaConta), 1);
+        	console.log(colors.fg.green, `\nA Conta numero: ${numero} foi apagada com sucesso!`, colors.reset);
+        } else
+        console.log(colors.fg.red, `\nA Conta numero: ${numero} não foi encontrada!`, colors.reset);
+    }
+
+    public sacar(numero: number, valor: number): void {
+        let conta = this.buscarNoArray(numero);
+		
+		if (conta != null) {
+			if (conta.sacar(valor) == true)
+                console.log(colors.fg.green,`\nO Saque na Conta numero: ${numero} foi efetuado com sucesso!`, colors.reset);		
+		} else
+            console.log(colors.fg.red,`\nA Conta numero: ${numero} não foi encontrada!`, colors.reset);
+    }
+
+    public depositar(numero: number, valor: number): void {
+        let conta = this.buscarNoArray(numero);
+		
+		if (conta != null) {
+			conta.depositar(valor);
+            console.log(colors.fg.green,`\nO Depósito na Conta numero: ${numero} foi efetuado com sucesso!`, colors.reset);		
+		} else
+            console.log(colors.fg.red,`\nA Conta numero: ${numero} não foi encontrada!`, colors.reset);
+    }
+
+    public transferir(numeroOrigem: number, numeroDestino: number, valor: number): void {
+        let contaOrigem = this.buscarNoArray(numeroOrigem);
+		let contaDestino = this.buscarNoArray(numeroDestino);
+
+		if (contaOrigem != null && contaDestino != null) {
+            if(contaOrigem.sacar(valor) == true) {
+                contaDestino.depositar(valor);
+                console.log(
+                    colors.fg.green, 
+                    `\nA Transferência da Conta numero: ${numeroOrigem} 
+                    para a Conta numero: ${numeroDestino} foi efetuada com sucesso!`, 
+                    colors.reset);		
+            }
+		
+		} else
+            console.log(
+                colors.fg.red, 
+                `\nA Conta numero: ${numeroOrigem} e/ou a Conta numero: ${numeroDestino} não foram encontradas!`, 
+                colors.reset);
+    }
+
+    public buscarNoArray(numero: number): Conta | null {
+        for (let conta of this.listaContas) {
+            if (conta.numero === numero) return conta;
+        }
+        return null;
+    }
+
+    public gerarNumero(): number {
+        return ++this.numero;
+    }
+}
